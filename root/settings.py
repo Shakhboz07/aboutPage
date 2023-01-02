92c6ec09828f42306d187cc2e73f7e2f4635da6c
Shakhboz07 <shahbozrakhmatov07@gmail.com>
2023-01-02 21:08:20 +0500

    procfile

--- root/settings.py
+++ root/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
