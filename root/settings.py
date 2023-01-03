aad61bd1c69692a926ec5835f79e7e15148e4e0d
Shakhboz07 <shahbozrakhmatov07@gmail.com>
2023-01-03 12:31:34 +0500

    procfile

--- root/settings.py
+++ root/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR ]
+        'DIRS': [BASE_DIR]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = ''
